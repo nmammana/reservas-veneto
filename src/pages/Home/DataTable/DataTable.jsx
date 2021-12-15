25f6da0ddbb67a7feef95f5da7c78b2ed2333222
Andrés Gallardo <gallardoandres18@gmail.com>
2021-12-14 22:00:54 -0300

    - se agrega columna estado - cambio la logica de como detectar propietario

--- src/pages/Home/DataTable/DataTable.jsx
+++ src/pages/Home/DataTable/DataTable.jsx
@@ -1,104 +1,115 @@
-import React from 'react'
-import MaterialTable from 'material-table';
-import tableIcons from '../../../assets/icons/material-icons/material-icons';
+import React from "react";
+import MaterialTable from "material-table";
+import tableIcons from "../../../assets/icons/material-icons/material-icons";
 
-import './DataTable.scss';
+import "./DataTable.scss";
 
-export default function DataTable({reservations}) {
-
+export default function DataTable({ reservations }) {
   const columns = [
     {
-      title: 'Fecha',
-      field: 'date',
+      title: "Fecha",
+      field: "date",
       searchable: false,
       filtering: true,
-     /*  filterPlaceholder: "Filtrar fecha..." */
-    },{
-      title: 'Nombre del propietario',
-      field: 'username',
+      /*  filterPlaceholder: "Filtrar fecha..." */
+    },
+    {
+      title: "Nombre del propietario",
+      field: "username",
       filtering: true,
       /* filterPlaceholder: "Filtrar nombre..." */
-    },{
-      title: 'Departamento',
-      field: 'apartment',
+    },
+    {
+      title: "Departamento",
+      field: "apartment",
       filtering: true,
       /* filterPlaceholder: "Filtrar depto..." */
-    },{
-      title: 'Tipo de reserva',
-      field: 'sport',
+    },
+    {
+      title: "Tipo de reserva",
+      field: "sport",
       searchable: false,
-      lookup: { 
-        'Fútbol cancha 1': 'Fútbol cancha 1', 
-        'Tenis cancha 1': 'Tenis cancha 1',
-        'Paddle cancha 1': 'Paddle cancha 1',
-        'Paddle cancha 2': 'Paddle cancha 2',
-        'Bochas cancha 1': 'Bochas cancha 1',
-        'Bochas cancha 2': 'Bochas cancha 2',
+      lookup: {
+        "Fútbol cancha 1": "Fútbol cancha 1",
+        "Tenis cancha 1": "Tenis cancha 1",
+        "Paddle cancha 1": "Paddle cancha 1",
+        "Paddle cancha 2": "Paddle cancha 2",
+        "Bochas cancha 1": "Bochas cancha 1",
+        "Bochas cancha 2": "Bochas cancha 2",
       },
-    },{
-      title: 'Propietario/ Inquilino',
-      field: 'hasIdentityNumber',
+    },
+    {
+      title: "Propietario/ Inquilino",
+      field: "hasIdentityNumber",
       searchable: false,
-      lookup: { 'Propietario': 'Propietario', 'Inquilino': 'Inquilino' },
-    },{
-      title: 'Documento',
-      field: 'identityNumber',
+      lookup: { Propietario: "Propietario", Inquilino: "Inquilino" },
+    },
+    {
+      title: "Documento",
+      field: "identityNumber",
       filtering: true,
       /* filterPlaceholder: "Filtrar DNI..." */
-    },{
-      title: 'Horas reservadas',
-      field: 'hours',
+    },
+    {
+      title: "Horas reservadas",
+      field: "hours",
       searchable: false,
       filtering: false,
-    }
+    },
+    {
+      title: "Estado",
+      field: "status",
+      lookup: { ACTIVA: "ACTIVA", CANCELADA: "CANCELADA" },
+    },
   ];
 
   return (
-        <MaterialTable
-          columns={
-            columns
-          }
-          data={reservations}
-          title=""
-          icons={tableIcons}
-          options={{
-            pageSize: 20,
-            search: false,
-            showTitle: false,
-            toolbar: false,
-            sorting: true,
-            filtering: true,
-            headerStyle: {
-              position: "sticky",
-              top: "0",
-              borderRadius: '10px',
-              height: '10px',
-            },
-            maxBodyHeight: "26rem",
-          }}
-          localization={{ 
-            toolbar: { 
-              searchPlaceholder: 'Buscar por nombre o DNI...',
-              searchTooltip: "Búsqueda por nombre o DNI"
-            },
-            pagination: {
-              labelRowsSelect: "Filas",
-              firstTooltip: "Primera página",
-              previousTooltip: "Anterior",
-              nextTooltip: "Siguiente",
-              lastTooltip: "Última página",
-              labelDisplayedRows: ``
-            },
-            body :{
-              filterRow: {
-                filterTooltip: 'Filtrar'
-              }
-            }
-          }}
-          className="material-table"
-          style={{
-            "borderRadius" : "10px"
-          }}     
-        />
-  )
+    <MaterialTable
+      columns={columns}
+      data={reservations}
+      title=""
+      icons={tableIcons}
+      options={{
+        pageSize: 20,
+        search: false,
+        showTitle: false,
+        toolbar: false,
+        sorting: true,
+        filtering: true,
+        headerStyle: {
+          position: "sticky",
+          top: "0",
+          borderRadius: "10px",
+          height: "10px",
+        },
+        maxBodyHeight: "26rem",
+      }}
+      localization={{
+        toolbar: {
+          searchPlaceholder: "Buscar por nombre o DNI...",
+          searchTooltip: "Búsqueda por nombre o DNI",
+        },
+        pagination: {
+          labelRowsSelect: "Filas",
+          firstTooltip: "Primera página",
+          previousTooltip: "Anterior",
+          nextTooltip: "Siguiente",
+          lastTooltip: "Última página",
+          labelDisplayedRows: ``,
+        },
+        body: {
+          filterRow: {
+            filterTooltip: "Filtrar",
+          },
+        },
+      }}
+      className="material-table"
+      style={{
+        borderRadius: "10px",
+        display: "grid",
+        height: "100vh",
+        gridTemplateRows: "auto 54px",
+      }}
+    />
+  );
 }
--- src/pages/Home/Home.jsx
+++ src/pages/Home/Home.jsx
@@ -1,86 +1,96 @@
-import React, { useEffect, useState } from 'react'
-import axios from 'axios';
-import Layout from '../../components/Layout'
-import './Home.scss';
-import DataTable from './DataTable/DataTable';
-import Loading from '../../components/Loading/Loading';
+import React, { useEffect, useState } from "react";
+import axios from "axios";
+import Layout from "../../components/Layout";
+import "./Home.scss";
+import DataTable from "./DataTable/DataTable";
+import Loading from "../../components/Loading/Loading";
 
 export default function Home() {
-    const [reservations, setReservations] = useState([])
+  const [reservations, setReservations] = useState([]);
 
-    useEffect(() => {
-        const fetchReservationsByUser = async () => {
-            const response = await axios.get(`http://${process.env.REACT_APP_API_URL}/reservation`);
-            if(response.data){
-                let reservationsData = [];
-                response.data.forEach(dataBlock => {
-                    let reservationData = {
-                        date : "",
-                        username: dataBlock.user.name,
-                        apartment: `Torre ${dataBlock.user.tower}, ${dataBlock.user.floor}°${dataBlock.user.apartment}`,
-                        sport: "",
-                        hasIdentityNumber: "",
-                        identityNumber: "",
-                        hours: `${dataBlock.hours[0]} a ${dataBlock.hours[dataBlock.hours.length-1]}`,
-                    }
+  useEffect(() => {
+    const fetchReservationsByUser = async () => {
+      const response = await axios.get(
+        `http://${process.env.REACT_APP_API_URL}/reservation`
+      );
+      if (response.data) {
+        let reservationsData = [];
+        response.data.forEach((dataBlock) => {
+          let reservationData = {
+            date: "",
+            username: dataBlock.user.owner,
+            apartment: `Torre ${dataBlock.user.tower}, ${dataBlock.user.floor}°${dataBlock.user.apartment}`,
+            sport: "",
+            hasIdentityNumber: "",
+            identityNumber: "",
+            hours: `${dataBlock.hours[0]} a ${
+              dataBlock.hours[dataBlock.hours.length - 1]
+            }`,
+            status: dataBlock.enabled ? "ACTIVA" : "CANCELADA",
+          };
 
-                    if(dataBlock.date.split('/')[0].length < 2){
-                        reservationData.date = "0".concat(dataBlock.date);
-                    }else{
-                        reservationData.date = dataBlock.date;
-                    }
+          if (dataBlock.date.split("/")[0].length < 2) {
+            reservationData.date = "0".concat(dataBlock.date);
+          } else {
+            reservationData.date = dataBlock.date;
+          }
 
-                    if(dataBlock.type.split('_')[0] === 'FOOTBALL'){
-                        reservationData.sport = "Fútbol";
-                    }else if(dataBlock.type.split('_')[0] === 'TENNIS'){
-                        reservationData.sport = "Tenis";
-                    }else if(dataBlock.type.split('_')[0] === 'PADDLE'){
-                        reservationData.sport = "Paddle";
-                    }else if(dataBlock.type.split('_')[0] === 'BOWLING'){
-                        reservationData.sport = "Bochas";
-                    }
-                    
-                    if(dataBlock.type.split('_')[1] === 'A'){
-                        reservationData.sport = reservationData.sport.concat(" ","cancha 1");
-                    }else if(dataBlock.type.split('_')[1] === 'B'){
-                        reservationData.sport = reservationData.sport.concat(" cancha 2");
-                    }else{
-                        reservationData.sport = reservationData.sport.concat(" cancha 1");
-                    }
-                    if(dataBlock.user.identityNumber !== "0" ){
-                        reservationData.identityNumber = String(dataBlock.user.identityNumber);
-                        reservationData.hasIdentityNumber = "Propietario"
-                    }else{
-                        reservationData.identityNumber = "";
-                        reservationData.hasIdentityNumber = "Inquilino"
-                    }
-                    reservationsData.push(reservationData)
-                })
-                setReservations(reservationsData)
-            }
-        }
-        fetchReservationsByUser();
-    }, [])
-   
-    return (
-        <Layout>
-            <main className="home-main">
-                <div className="selection-container">
-                    <div className="rectangle-container">
-                        <div className="rectangle"></div>
-                    </div>
-                    <div className="home-main__wrapper">
-                        <div className="heading-container">
-                            <h3 className="title heading2">Reservas del área deportiva</h3>
-                        </div>
-                        {reservations?.length > 0 ? 
-                            <DataTable reservations={reservations}/> 
-                        :
-                            <Loading/>
-                        }
-                    </div>
-                </div>
-            </main>
-        </Layout>
-    )
+          if (dataBlock.type.split("_")[0] === "FOOTBALL") {
+            reservationData.sport = "Fútbol";
+          } else if (dataBlock.type.split("_")[0] === "TENNIS") {
+            reservationData.sport = "Tenis";
+          } else if (dataBlock.type.split("_")[0] === "PADDLE") {
+            reservationData.sport = "Paddle";
+          } else if (dataBlock.type.split("_")[0] === "BOWLING") {
+            reservationData.sport = "Bochas";
+          }
+
+          if (dataBlock.type.split("_")[1] === "A") {
+            reservationData.sport = reservationData.sport.concat(
+              " ",
+              "cancha 1"
+            );
+          } else if (dataBlock.type.split("_")[1] === "B") {
+            reservationData.sport = reservationData.sport.concat(" cancha 2");
+          } else {
+            reservationData.sport = reservationData.sport.concat(" cancha 1");
+          }
+          if (dataBlock.user.name) {
+            reservationData.identityNumber = String(
+              dataBlock.user.identityNumber
+            );
+            reservationData.hasIdentityNumber = "Propietario";
+          } else {
+            reservationData.identityNumber = "";
+            reservationData.hasIdentityNumber = "Inquilino";
+          }
+          reservationsData.push(reservationData);
+        });
+        setReservations(reservationsData);
+      }
+    };
+    fetchReservationsByUser();
+  }, []);
+
+  return (
+    <Layout>
+      <main className="home-main">
+        <div className="selection-container">
+          <div className="rectangle-container">
+            <div className="rectangle"></div>
+          </div>
+          <div className="home-main__wrapper">
+            <div className="heading-container">
+              <h3 className="title heading2">Reservas del área deportiva</h3>
+            </div>
+            {reservations?.length > 0 ? (
+              <DataTable reservations={reservations} />
+            ) : (
+              <Loading />
+            )}
+          </div>
+        </div>
+      </main>
+    </Layout>
+  );
 }
